1ff50e9c72cee333a11046a44fe160f345049a69
talhabinzafar <talhabinzafar@yahoo.com>
2020-02-28 01:58:41 +0500

    Added Form layout...

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -2,5 +2,14 @@
 /* @var $this SiteController */
 
 ?>
-
-<h2>Index Page</h2>
+<div class="container">
+    <div class="col-md-6">
+        <form class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal">Add User</h1>
+            <input name="name" class="form-control" placeholder="Name"><br>
+            <input name="email" class="form-control" placeholder="Email address"><br>
+            <textarea name="address" cols="66" rows="10"></textarea> <br><br>
+            <button class="btn btn-primary" type="submit">Create</button>
+        </form>
+    </div>
+</div>
