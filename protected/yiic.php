678a15ded62cf37cb65ad4dd841ef3fe3721c06f
talhabinzafar <talhabinzafar@yahoo.com>
2020-02-28 01:14:45 +0500

    Initial cleanup and addition..

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,8 @@
 <?php
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+//$yiic=dirname(__FILE__).'/../../yii/framework/yiic.php';
+$yiic = dirname(__FILE__) . '/../../YiiRoot/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
