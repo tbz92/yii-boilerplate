c68f90bcaf869050ce38cfa065b172c16173eed9
talhabinzafar <talhabinzafar@yahoo.com>
2020-02-28 21:28:30 +0500

    Minor updates and new model

--- protected/migrations/m200227_201654_createUserTable.php
+++ protected/migrations/m200227_201654_createUserTable.php
@@ -7,8 +7,12 @@ class m200227_201654_createUserTable extends CDbMigration
         $this->createTable('users', [
             'id' => 'int(11) not null primary key auto_increment',
             'name' => 'varchar(50) not null',
-            'email' => 'varchar(50) not null',
+            'email' => 'varchar(50) not null unique',
             'address' => 'text default null',
+            'created_by' => 'varchar(100) not null',
+            'created_at' => 'timestamp not null default current_timestamp',
+            'updated_by' => 'varchar(100) not null',
+            'updated_at' => 'timestamp not null default "0000-00-00 00:00:00"',
         ]);
     }
 
---
+++ protected/models/User.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * This is the model class for table "user".
+ *
+ * The followings are the available columns in table 'user':
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $address
+ * @property string $created_by
+ * @property string $created_at
+ * @property string $updated_by
+ * @property string $updated_at
+ */
+class User extends CFormModel
+{
+    public function __construct($scenario = 'insert')
+    {
+        parent::__construct($scenario);
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className
+     * record class name.
+     * @return User the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('name, email', 'required'),
+            array('name, email', 'length', 'max' => 50),
+            array('email', 'email'),
+            array('email', 'unique', 'message' => 'This email address is unavailable.'),
+            array('id, name, email, address', 'safe', 'on' => 'search'),
+        );
+    }
+
+    public function findByEmail($email)
+    {
+        $model = $this->find(array(
+            'condition' => "email = :email",
+            'params' => array(':email' => $email)
+        ));
+
+        return $model;
+    }
+
+    protected function beforeValidate()
+    {
+        if (parent::beforeValidate()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'created_by' => 'Created By',
+            'created_at' => 'Created At',
+            'updated_by' => 'Updated By',
+            'updated_at' => 'Updated At',
+        );
+    }
+
+    public function search()
+    {
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('id', $this->id);
+        $criteria->compare('name', $this->name, true);
+        $criteria->compare('email', $this->email);
+        $criteria->compare('address', $this->address);
+        $criteria->compare('updated_by', $this->updated_by, true);
+        $criteria->compare('updated_at', $this->updated_at, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    protected function afterFind()
+    {
+        parent::afterFind();
+    }
+
+    protected function beforeSave()
+    {
+        return parent::beforeSave();
+    }
+
+    protected function afterSave()
+    {
+        parent::afterSave();
+    }
+}
